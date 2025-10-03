c2a441b25948fc94e424ba4657030e006cc7de28
Kev <43802321+KevYRS@users.noreply.github.com>
2025-10-03 14:00:56 -0600

    Update Error.cs

--- Error.cs
+++ Error.cs
@@ -3,17 +3,12 @@ using System.IO;
 using System.Collections.Generic;
 using System.Text;
 
-namespace sintaxis3
-{
-    class Error : Exception
-    {
-        public Error()
-        {
+namespace sintaxis3{
+    class Error : Exception{
+        public Error(){
 
         }
-
-        public Error(StreamWriter bitacora, string error) : base(error)
-        {
+        public Error(StreamWriter bitacora, string error) : base(error){
             //string mensaje = error + " linea " + linea + " caracter " + caracter;
             bitacora.WriteLine(error);
         }
